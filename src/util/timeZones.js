40681bc9d99a38e81b1b25f01b5b9478507712c5
adva <adva.apelboim@gmail.com>
2024-08-12 18:36:30 +0300

    ready-without-edit-function

--- src/util/timeZones.js
+++ src/util/timeZones.js
@@ -1,5 +1,6 @@
 export const timeZones = [
   { label: "UTC (Universal Time Coordinated)", ianaName: "UTC" },
+  { label: "IST (Israel Standard Time)", ianaName: "Asia/Jerusalem" },
   { label: "EST (Eastern Standard Time)", ianaName: "America/New_York" },
   { label: "CST (Central Standard Time)", ianaName: "America/Chicago" },
   { label: "MST (Mountain Standard Time)", ianaName: "America/Denver" },
@@ -7,9 +8,6 @@ export const timeZones = [
   { label: "GMT (Greenwich Mean Time)", ianaName: "Europe/London" },
   { label: "BST (British Summer Time)", ianaName: "Europe/London" },
   { label: "CET (Central European Time)", ianaName: "Europe/Paris" },
-  { label: "IST (Israel Standard Time)", ianaName: "Asia/Jerusalem" },
-  { label: "AST (Arabian Standard Time)", ianaName: "Asia/Riyadh" },
-  { label: "IST (India Standard Time)", ianaName: "Asia/Kolkata" },
   { label: "CST (China Standard Time)", ianaName: "Asia/Shanghai" },
   { label: "JST (Japan Standard Time)", ianaName: "Asia/Tokyo" },
 ];
